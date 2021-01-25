d982b68ba7641a3fc99706f75fba70a3f237d1a6
Sergey Biryukov <2700036@mail.ru>
2021-01-25 23:10:33 +0300

    upd

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,4 +1,4 @@
-export const baseUrl = 'https://jobs.github.com/positions.json?'
+export const baseUrl = 'https://cors-anywhere.herokuapp.com/https://jobs.github.com/positions.json?'
 
 // https://jobs.github.com/positions.json?
 // https://cors-anywhere.herokuapp.com/
